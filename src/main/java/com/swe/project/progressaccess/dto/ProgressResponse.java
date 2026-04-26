bc59022935bb0e1b1abbac6290d9c80eace26e3f
jurzheng <jurzheng@iu.edu>
2026-04-26 12:58:50 -0400

    ProgressResonse DTO now usable

--- src/main/java/com/swe/project/progressaccess/dto/ProgressResponse.java
+++ src/main/java/com/swe/project/progressaccess/dto/ProgressResponse.java
@@ -1,5 +1,20 @@
 package com.swe.project.progressaccess.dto;
 
-public class ProgressResponse {
+import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgressResponse {
+    private Long id;
+
+    private String learnerId;
+    private String topicId;
+    private String label;
+
+    private LocalDateTime createdAt;
 }
